2f980143b168d45051cfa22d4e1dd27ec2de7abd
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-08-07 01:25:29 -0500

    move files

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //28miniproject copy
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');  //not sure what this is for
-const api = require('./routes/index.js');
+// const { clog } = require('./middleware/clog');  //not sure what this is for
+const api = require('./Develop/routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 // Import custom middleware, "cLog"
-app.use(clog);  //not sure if I need this
+// app.use(clog);  //not sure if I need this
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
